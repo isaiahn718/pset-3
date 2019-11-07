804367d4cb49b68596ef5817c738a1d3f0e71e52
isaiahn718 <55455558+isaiahn718@users.noreply.github.com>
2019-11-06 20:38:36 -0500

    sixth

---
+++ src/gpa.js
@@ -0,0 +1,43 @@
+const readlineSync = require("readline-sync");
+
+const grade = String(readlineSync.question("\nEnter a letter grade: "));
+
+if (grade == "A+" || grade == "A" || grade == "a+" || grade == "a") {
+      console.log("\nYour GPA is 4.00.")
+
+} else if  (grade == "A-" || grade == "a-") {
+      console.log("\nYour GPA is 3.67.")
+
+} else if  (grade == "B+" || grade == "b+") {
+      console.log("\nYour GPA is 3.33.")
+
+} else if  (grade == "B"  || grade == "b") {
+      console.log("\nYour GPA is 3.00.")
+
+} else if  (grade == "B-" || grade == "b-") {
+      console.log("\nYour GPA is 2.67.")
+
+} else if  (grade == "C+" || grade == "c+") {
+      console.log("\nYour GPA is 2.33.")
+
+} else if  (grade == "C"  || grade == "c") {
+      console.log("\nYour GPA is 2.00.")
+
+} else if  (grade == "C-" || grade == "c-") {
+      console.log("\nYour GPA is 1.67.")
+
+} else if  (grade == "D+" || grade == "d+") {
+      console.log("\nYour GPA is 1.33.")
+
+} else if  (grade == "D"  || grade == "d") {
+      console.log("\nYour GPA is 1.00.")
+
+} else if  (grade == "D-" || grade == "d-") {
+      console.log("\nYour GPA is 0.67.")
+
+} else if  (grade == "F"  || grade == "f") {
+      console.log("\nYour GPA is 0.00.")
+
+} else {
+      console.log("\nInvalid.")
+}
