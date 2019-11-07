2ed5141b94603086d6234f2f0ec3ccb9834afb9d
isaiahn718 <55455558+isaiahn718@users.noreply.github.com>
2019-11-06 19:47:58 -0500

    fourth

---
+++ src/parity.js
@@ -0,0 +1,25 @@
+const readlineSync = require("readline-sync");
+
+const integer= Number(readlineSync.question("\nEnter an integer: "));
+
+if (!Number.isInteger(integer)) {
+
+   console.log("\nInvalid.\n")
+
+} else if (integer < Number.MIN_SAFE_INTEGER || integer > Number.MAX_SAFE_INTEGER) {
+
+   console.log("\nInvalid.\n")
+
+} else if (integer % 2 == 0) {
+
+   console.log("\nEven.\n")
+
+} else if (integer % 2 == 1 || integer % 2 == (-1)) {
+
+   console.log("\nOdd.\n")
+
+} else {
+
+   console.log("\nInvalid.\n")
+
+}
