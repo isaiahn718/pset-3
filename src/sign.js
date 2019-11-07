2652e45582d8fa7823b4ac83512e1aff5161d258
isaiahn718 <55455558+isaiahn718@users.noreply.github.com>
2019-11-06 19:39:21 -0500

    third

---
+++ src/sign.js
@@ -0,0 +1,17 @@
+const readlineSync = require("readline-sync");
+
+let number = Number(readlineSync.question("\nEnter a number: "));
+
+if (Number.isNaN(number)) {
+    console.log("\nInvalid.");
+} else if (number < Number.MIN_SAFE_INTEGER || number > Number.MAX_SAFE_INTEGER) {
+    console.log("\nInvalid.");
+} else if (number<0) {
+      console.log("\nNegative.")
+    }
+    else if (number>0) {
+      console.log("\nPositive.")
+    }
+    else {
+      console.log("\nZero.")
+    }
